8ecdbcdd6d6d8ca903abd9024ccc284fb55f015f
Olapat <lapatbr19@gmail.com>
2018-12-19 01:17:44 +0700

    setup

---
+++ scr/form_interest_product.js
@@ -0,0 +1,167 @@
+
+// เพิ่มข้อมูล
+// let con = firebase.database().ref('data');
+// function saveData(){
+//     let getAge = document.getElementById('age').value;
+//     let getSex = document.getElementById('sex').value;
+//     let getStatus = document.getElementById('status').value;
+//     let getLorW =  document.getElementById('l_or_w').value;
+//     let getProduct =  document.getElementById('product').value;
+
+//     let data = {
+//         age: getAge,
+//         sex: getSex,
+//         status: getStatus,
+//         l_or_w: getLorW,
+//         product: getProduct
+//     }
+//     con.push(data);
+//     console.log(data)
+// }
+
+
+
+// รับ เตรียมข้อมูล
+// function getData() {
+    // con.on('value', gotData, errdata);
+    //  function gotData(data) {
+    //         Data = data.val();
+    //     // console.log(Data)
+    //         for (let va of Object.entries(Data)) {
+    //             let val = va[1]
+    //             dataAll.push([val.age, val.sex, val.l_or_w, val.status]);
+    //             interest.push(val.product);
+    //         }
+    //     // console.log(dataAll);
+    //     console.log(interest);
+    // }
+    // function errdata(err) {
+    //     console.log("err: "+ err);
+    // } 
+// }
+let dataJ = d; //from form-interest-product-export.js
+let dataJson;
+let dataAll = [];
+let interest = [];  
+let keys = Object.keys(dataJ);
+let labalProduct = ['อุปกรณ์คอม', 'อาหารเสริม', 'แฟชั่น', 'ของใช้ในบ้าน'];
+for (let key of keys) {
+    let val = dataJ[key];
+    let ageInt = parseInt(val.age)
+    let ageFlost = ageInt / 60;
+    let keySex = val.sex === 'ชาย' ? 0 : 1 
+    let keyLW = val.l_or_w === 'เรียน' ? 0 : 1
+    let keyStatus = val.status === 'โสด' ? 0 : 1
+    let keyProduct = val.product === 'อุปกรณ์คอม' ? 0 : 
+    val.product === 'อาหารเสริม' ? 1 : 
+    val.product === 'แฟชั่น' ? 2 : 3 // 3 = ของใช้ในบ้าน
+    let d = [ageFlost, keySex, keyLW, keyStatus]
+    dataAll.push(d)
+    interest.push(keyProduct);
+}
+
+// console.log(dataAll);
+// console.log(interest);
+
+//ออกแบบ tensor xs, ys
+let xs = tf.tensor2d(dataAll);
+// xs.print();
+
+let interest1D = tf.tensor1d(interest, 'int32');
+// interest1D.print();
+
+let ys = tf.oneHot(interest1D, 4).cast('float32');
+// ys.print();
+interest1D.dispose();
+
+let model = tf.sequential();
+
+const hidden = tf.layers.dense({
+    units: 4,
+    inputShape: [4],
+    activation: 'sigmoid'
+});
+
+const output = tf.layers.dense({
+    units: 4,
+    activation: 'softmax'
+});
+
+model.add(hidden);
+model.add(output);
+
+let optimizer = tf.train.sgd(0.2);
+model.compile({
+    optimizer: optimizer,
+    loss: 'categoricalCrossentropy',
+    metrics: ['accuracy']
+});
+
+async function train() {
+    await model.fit(xs, ys, {
+        validationSplit: 0,
+        epochs: 2000, 
+        callbacks: {
+            onEpochEnd: (e, l) => {
+                console.log(e, ' ', l.loss);
+            },
+            onTrainEnd: () => {
+                console.log("เทรดเสร็จแล้ว");
+            }
+        }
+    });
+}
+
+train();
+
+function getQQ() {
+    let getAge = document.getElementById('age').value;
+    let getSex = document.getElementById('sex').value;
+    let getStatus = document.getElementById('status').value;
+    let getLorW =  document.getElementById('l_or_w').value;
+
+    let getAgeInt = parseInt(getAge);
+    let getKeySex = getSex === 'ชาย' ? 0 : 1 ;
+    let getKeyLW = getLorW === 'เรียน' ? 0 : 1;
+    let getKeyStatus = getStatus === 'โสด' ? 0 : 1;
+
+    let test2D = [
+        [getAgeInt / 60, getKeySex, getKeyLW, getKeyStatus]
+    ]
+
+    let totensor2D = tf.tensor2d(test2D);
+    // console.log(totensor2D);
+    toAi(totensor2D);
+}
+
+function toAi(dataTest) {
+    tf.tidy(() => {
+        let results = model.predict(dataTest);
+        results.print();
+        let resultsDataSync = results.dataSync();
+        let chat = Array.from(resultsDataSync);
+        // let argMax = results.argMax(1);
+        // let indexProductOfTest =  argMax.dataSync();
+        // let topProduct = labalProduct[indexProductOfTest];
+        // console.log(topProduct);
+        let chatMaxToMin = chat.slice(); //clone
+
+        chatMaxToMin.sort(function(a, b){ //เรียง ม - น
+            return b-a
+        });
+        // console.log(chat);
+        // console.log(chatMaxToMin);
+        let displayChatLabel = [];
+        let displayChatPersen = [];
+        chatMaxToMin.forEach((va) => {
+            let indexLabalProduct = chat.indexOf(va)
+            let persen = va * 100
+            displayChatLabel.push('<b>'+labalProduct[indexLabalProduct]+'</b> <br/>');
+            displayChatPersen.push(persen.toFixed(2)+'<br/>');     
+            let displayLabel = document.getElementById('label');
+            let displayPersen = document.getElementById('persen');
+            displayLabel.innerHTML = displayChatLabel.join('')
+            displayPersen.innerHTML = displayChatPersen.join('')
+        });
+    }) 
+}
---
+++ scr/index.js
@@ -0,0 +1,187 @@
+﻿// let aVGA = answersVGA_Card;
+// let aram = answersRam;
+// let aAll = answers;
+// let testInput = input_3D;
+// let testaAll_2d = [];
+// let testaAll_1d = [];
+
+// let qProduct = question_product;
+// let qDeProduct = question_detail_product;
+
+// let model;
+// let qIndexP = [];
+// let qIndexPD = [];
+// let qIndexOneHotP = [];
+// let qIndexOneHotPD = [];
+// let indexResulte = [];
+
+
+// for (let i = 0; i < qProduct.length; i++) {
+//     let y = i / qProduct.length;
+//     qIndexP.push([y]);
+//     qIndexOneHotP.push(i);
+// }
+
+// for (let i = 0; i < qDeProduct.length; i++) {
+//     let y = i / qDeProduct.length;
+//     qIndexPD.push([y]);
+//     qIndexOneHotPD.push(i);
+// }
+
+// for (let i = 0; i < aAll.length; i++) {
+//     let y = i / aAll.length;
+//     testaAll_2d.push([y]);
+//     testaAll_1d.push(i);
+// }
+
+// let xs = tf.tensor2d(qIndexP);
+// xs.print();
+// let indexTensorP = tf.tensor1d(qIndexOneHotP, 'int32');
+// indexTensorP.print();
+// indexTensorP.dispose();
+
+
+// let ys = tf.oneHot(indexTensorP, qProduct.length).cast('float32');
+// ys.print();
+
+
+// console.log(testaAll_1d);
+// let testInput_1done = tf.tensor1d(testaAll_1d, 'int32');
+// testInput_1done.print();
+// let ysAllasw = tf.oneHot(testInput_1done, testaAll_1d.length).cast('float32');
+// ysAllasw.print();
+
+// let qet = tf.tensor3d(aAll);
+// qet.print();
+
+// let indexTensorPD = tf.tensor1d(qIndexOneHotPD, 'int32');
+// let ys2 = tf.oneHot(indexTensorPD, qDeProduct.length).cast('float32');
+// // ys2.print();
+
+// model = tf.sequential();
+// const hidden = tf.layers.dense({ //เข้า Product
+//     units: 4,
+//     inputShape: [1],
+//     activation: 'sigmoid'
+// });
+
+// const output = tf.layers.dense({
+//     units: 4,
+//     activation: 'softmax'
+// });
+
+// // const hidden2 = tf.layers.dense({ //เข้า คำถาม
+// //     units: 4,
+// //     inputShape: [4],
+// //     activation: 'sigmoid'
+// // });
+
+// model.add(hidden);
+// // model.add(hidden2);
+// model.add(output);
+
+
+// const optimizer = tf.train.sgd(1.0);
+
+// model.compile({
+//     optimizer: optimizer,
+//     loss: 'categoricalCrossentropy',
+//     matrics: ['accuracy']
+// });
+
+// function draw() {
+//     let rr = []
+//     tf.tidy(() => {
+//         const input = tf.tensor2d(qIndexP);
+//         let resulte = model.predict(input);
+//         resulte.print();
+//         let argMax = resulte.argMax(1);
+//         // argMax.print();
+        
+//         for (let i = 0; i < qProduct.length; i++) {
+//             let ll = argMax.dataSync()[i];
+//             rr.push(ll);
+//         }
+//         // console.log(rr);
+        
+//     });
+//     return rr;
+// };
+
+// // train();
+
+// async function train () {
+//     await model.fit(xs, ys, {
+//         validationSplit: 0,
+//         epochs: 1,
+//         callbacks: {
+//             onEpochEnd: (epoch, logs) => {
+//                 console.log(epoch);
+//                 console.log(logs.loss);
+//             },
+//             // onBatchEnd: async () => {
+//             //     await tf.nextFrame();
+//             // },
+//             onTrainEnd: () => {
+//                 console.log("เสร็จ");
+//             }
+//         }
+//     });
+
+//    indexResulte = draw();
+// }
+
+// function getinput() {
+//     let input = document.getElementById('input').value;
+//     let i= 0;
+//     let d = -1;
+//     let t, tt;
+
+//     while (d === -1) {
+//         d = input.search(qProduct[i])
+//         t = i;
+//         i++;
+//     }
+//     if (t >= qProduct.length) {
+//         console.log("ไม่เจอ");
+//     }else {
+//         detail(input, t);
+//         // model2(t,tt);
+//     }
+// };
+
+// let td = 0;
+// function detail(input, t) {
+//     let d = -1;
+//     let i= 0;
+//     let te;
+    
+//     while (d === -1) {
+//         d = input.search(qDeProduct[t][td][i])
+//         if (i === qDeProduct[t][td].length) {
+//             td++;
+//         }
+//         te = i;
+//         i++;
+//         console.log("td ", td)
+//     }
+//     if (te >= qDeProduct.length && d === -1) {
+//         console.log("ไม่เจอP");
+//     }else {
+//         // model2(te);
+//         console.log("เจอแล้ว ", t , " " , td, );
+//         console.log(qDeProduct[t][td])
+//         console.log(aAll[t][td])
+//         td = 0;
+//     }
+// }
+
+// function model2(indexP, indexPD) {
+//     // console.log(indexResulte);
+//     function findFirstLargeNumber(element) {
+//         return element === indexP;
+//       }
+//     let indexSearchP  = qProduct.findIndex(findFirstLargeNumber);
+//     console.log("เจอ " , indexSearchP);
+//     let indexSearchPD = qDeProduct.findIndex(findFirstLargeNumber);
+// }
