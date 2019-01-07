f6796a5e1df76177670c6361b7f5d17e2bb6699f
Olapat <lapatbr19@gmail.com>
2019-01-08 03:02:11 +0700

    good!
    e: 1000
    acc: 0.9615383744239807

---
+++ data-set/form-interest-product-data-set-export.js
@@ -0,0 +1,795 @@
+let DATA =
+{
+    "-LVd0gogzuVFDXv9ViG3": {
+        "age": "12",
+        "product": "เกมและซอฟแวร์",
+        "sex": "ชาย",
+        "status": "โสด"
+    },
+    "-LVd0jdPEZum7QCp_1BR": {
+        "age": "13",
+        "product": "เกมและซอฟแวร์",
+        "sex": "ชาย",
+        "status": "โสด"
+    },
+    "-LVd0kRXfIbIO_Z7TPiZ": {
+        "age": "14",
+        "product": "เกมและซอฟแวร์",
+        "sex": "ชาย",
+        "status": "โสด"
+    },
+    "-LVd0kzfS6WCHvQeiYhV": {
+        "age": "15",
+        "product": "เกมและซอฟแวร์",
+        "sex": "ชาย",
+        "status": "โสด"
+    },
+    "-LVd0lgHwsNOXwUIGoS8": {
+        "age": "16",
+        "product": "เกมและซอฟแวร์",
+        "sex": "ชาย",
+        "status": "โสด"
+    },
+    "-LVd0mAHSv7DCk_QJgNo": {
+        "age": "17",
+        "product": "เกมและซอฟแวร์",
+        "sex": "ชาย",
+        "status": "โสด"
+    },
+    "-LVd0mePat8Hra1AJXTq": {
+        "age": "18",
+        "product": "เกมและซอฟแวร์",
+        "sex": "ชาย",
+        "status": "โสด"
+    },
+    "-LVd0nG1ci0-DKClZyBM": {
+        "age": "19",
+        "product": "เกมและซอฟแวร์",
+        "sex": "ชาย",
+        "status": "โสด"
+    },
+    "-LVd0ohPo0v49scoFhCF": {
+        "age": "20",
+        "product": "เกมและซอฟแวร์",
+        "sex": "ชาย",
+        "status": "โสด"
+    },
+    "-LVd0pfYvLbGg_DahpDm": {
+        "age": "21",
+        "product": "เกมและซอฟแวร์",
+        "sex": "ชาย",
+        "status": "โสด"
+    },
+    "-LVd0qJXb_SaeuNWANEB": {
+        "age": "22",
+        "product": "เกมและซอฟแวร์",
+        "sex": "ชาย",
+        "status": "โสด"
+    },
+    "-LVd0sA1cGQcJYIeXZgf": {
+        "age": "23",
+        "product": "แฟชั่น",
+        "sex": "ชาย",
+        "status": "โสด"
+    },
+    "-LVd0sxurmdSvTWSWfcl": {
+        "age": "24",
+        "product": "แฟชั่น",
+        "sex": "ชาย",
+        "status": "โสด"
+    },
+    "-LVd0t_HHCb4T9qy5XyU": {
+        "age": "25",
+        "product": "แฟชั่น",
+        "sex": "ชาย",
+        "status": "โสด"
+    },
+    "-LVd0ugPns2aGdG5ynD1": {
+        "age": "26",
+        "product": "แฟชั่น",
+        "sex": "ชาย",
+        "status": "โสด"
+    },
+    "-LVd0vAm9a4avz6zobDk": {
+        "age": "27",
+        "product": "แฟชั่น",
+        "sex": "ชาย",
+        "status": "โสด"
+    },
+    "-LVd0vdfJsDf1foc1k7F": {
+        "age": "28",
+        "product": "แฟชั่น",
+        "sex": "ชาย",
+        "status": "โสด"
+    },
+    "-LVd0w2HeNaY579EKmuK": {
+        "age": "29",
+        "product": "แฟชั่น",
+        "sex": "ชาย",
+        "status": "โสด"
+    },
+    "-LVd0xDXr-rgpjh0iBz7": {
+        "age": "30",
+        "product": "อุปกรณ์อิเล็กทรอนิกส์",
+        "sex": "ชาย",
+        "status": "โสด"
+    },
+    "-LVd0yF9cbDkmt9c3bwt": {
+        "age": "31",
+        "product": "อุปกรณ์อิเล็กทรอนิกส์",
+        "sex": "ชาย",
+        "status": "โสด"
+    },
+    "-LVd0ykeAKU4hDCigmBW": {
+        "age": "32",
+        "product": "อุปกรณ์อิเล็กทรอนิกส์",
+        "sex": "ชาย",
+        "status": "โสด"
+    },
+    "-LVd1-wv25hICL1g_FuF": {
+        "age": "33",
+        "product": "อุปกรณ์อิเล็กทรอนิกส์",
+        "sex": "ชาย",
+        "status": "โสด"
+    },
+    "-LVd10M1b0Q-TTX7F9yx": {
+        "age": "34",
+        "product": "อุปกรณ์อิเล็กทรอนิกส์",
+        "sex": "ชาย",
+        "status": "โสด"
+    },
+    "-LVd10r2lLXJYkL_UnfB": {
+        "age": "35",
+        "product": "อุปกรณ์อิเล็กทรอนิกส์",
+        "sex": "ชาย",
+        "status": "โสด"
+    },
+    "-LVd11LPKnGbXtZDYIuk": {
+        "age": "36",
+        "product": "อุปกรณ์อิเล็กทรอนิกส์",
+        "sex": "ชาย",
+        "status": "โสด"
+    },
+    "-LVd11huF0LXEtoMgNjS": {
+        "age": "37",
+        "product": "อุปกรณ์อิเล็กทรอนิกส์",
+        "sex": "ชาย",
+        "status": "โสด"
+    },
+    "-LVd12WAWaoe9zEUTiVP": {
+        "age": "38",
+        "product": "อุปกรณ์อิเล็กทรอนิกส์",
+        "sex": "ชาย",
+        "status": "โสด"
+    },
+    "-LVd12u9k4rMGlc8PJ6n": {
+        "age": "39",
+        "product": "อุปกรณ์อิเล็กทรอนิกส์",
+        "sex": "ชาย",
+        "status": "โสด"
+    },
+    "-LVd165v3wdDptU_qIJl": {
+        "age": "40",
+        "product": "สุขภาพและความงาม",
+        "sex": "ชาย",
+        "status": "โสด"
+    },
+    "-LVd178X80oNheLFtamz": {
+        "age": "41",
+        "product": "สุขภาพและความงาม",
+        "sex": "ชาย",
+        "status": "โสด"
+    },
+    "-LVd18EPHW6hYAbgfBNn": {
+        "age": "42",
+        "product": "สุขภาพและความงาม",
+        "sex": "ชาย",
+        "status": "โสด"
+    },
+    "-LVd18oAsHOdaoFA2Wmh": {
+        "age": "43",
+        "product": "สุขภาพและความงาม",
+        "sex": "ชาย",
+        "status": "โสด"
+    },
+    "-LVd19K1_NmPAowawrrH": {
+        "age": "44",
+        "product": "สุขภาพและความงาม",
+        "sex": "ชาย",
+        "status": "โสด"
+    },
+    "-LVd19hmztI1_ClJ3BOL": {
+        "age": "45",
+        "product": "สุขภาพและความงาม",
+        "sex": "ชาย",
+        "status": "โสด"
+    },
+    "-LVd1A5e8RkHvo0_k8x-": {
+        "age": "46",
+        "product": "สุขภาพและความงาม",
+        "sex": "ชาย",
+        "status": "โสด"
+    },
+    "-LVd1AjXSKeHDiWavxLg": {
+        "age": "47",
+        "product": "สุขภาพและความงาม",
+        "sex": "ชาย",
+        "status": "โสด"
+    },
+    "-LVd1BEHSBJfLGXawVVH": {
+        "age": "48",
+        "product": "สุขภาพและความงาม",
+        "sex": "ชาย",
+        "status": "โสด"
+    },
+    "-LVd1BtuM-nglrqgf_C5": {
+        "age": "49",
+        "product": "สุขภาพและความงาม",
+        "sex": "ชาย",
+        "status": "โสด"
+    },
+    "-LVd1CqX8fGYkawVqWqs": {
+        "age": "50",
+        "product": "ของใช้ในบ้านและเครื่องมือต่างๆ",
+        "sex": "ชาย",
+        "status": "โสด"
+    },
+    "-LVd1Dm9I5N4XJc-vK7W": {
+        "age": "51",
+        "product": "ของใช้ในบ้านและเครื่องมือต่างๆ",
+        "sex": "ชาย",
+        "status": "โสด"
+    },
+    "-LVd1EeHVIxRwnV91N_A": {
+        "age": "52",
+        "product": "ของใช้ในบ้านและเครื่องมือต่างๆ",
+        "sex": "ชาย",
+        "status": "โสด"
+    },
+    "-LVd1F8weekHm6HKTlhQ": {
+        "age": "53",
+        "product": "ของใช้ในบ้านและเครื่องมือต่างๆ",
+        "sex": "ชาย",
+        "status": "โสด"
+    },
+    "-LVd1Fx2M8_xsGgRbCvA": {
+        "age": "54",
+        "product": "ของใช้ในบ้านและเครื่องมือต่างๆ",
+        "sex": "ชาย",
+        "status": "โสด"
+    },
+    "-LVd1GbvVvRwB5mpaqJL": {
+        "age": "55",
+        "product": "ของใช้ในบ้านและเครื่องมือต่างๆ",
+        "sex": "ชาย",
+        "status": "โสด"
+    },
+    "-LVd1LQ0jImqp-GMzLHI": {
+        "age": "12",
+        "product": "เกมและซอฟแวร์",
+        "sex": "ชาย",
+        "status": "มีแฟน"
+    },
+    "-LVd1N5AoYAQDXzPCryB": {
+        "age": "13",
+        "product": "เกมและซอฟแวร์",
+        "sex": "ชาย",
+        "status": "มีแฟน"
+    },
+    "-LVd1Nv2bRgo9ESMIMtO": {
+        "age": "14",
+        "product": "เกมและซอฟแวร์",
+        "sex": "ชาย",
+        "status": "มีแฟน"
+    },
+    "-LVd1OeYJS0GppYNpwAU": {
+        "age": "15",
+        "product": "เกมและซอฟแวร์",
+        "sex": "ชาย",
+        "status": "มีแฟน"
+    },
+    "-LVd1PaYE4wHBRN3oIaZ": {
+        "age": "16",
+        "product": "เกมและซอฟแวร์",
+        "sex": "ชาย",
+        "status": "มีแฟน"
+    },
+    "-LVd1QIY3-7mj4aAtbYZ": {
+        "age": "17",
+        "product": "เกมและซอฟแวร์",
+        "sex": "ชาย",
+        "status": "มีแฟน"
+    },
+    "-LVd1R6XS59UXo93WtSM": {
+        "age": "18",
+        "product": "เกมและซอฟแวร์",
+        "sex": "ชาย",
+        "status": "มีแฟน"
+    },
+    "-LVd1RpHkcq-HysSVUrF": {
+        "age": "19",
+        "product": "เกมและซอฟแวร์",
+        "sex": "ชาย",
+        "status": "มีแฟน"
+    },
+    "-LVd1SXI5ixkNnMq5UHf": {
+        "age": "20",
+        "product": "เกมและซอฟแวร์",
+        "sex": "ชาย",
+        "status": "มีแฟน"
+    },
+    "-LVd1TpfUkE5OsvhrzTu": {
+        "age": "21",
+        "product": "เกมและซอฟแวร์",
+        "sex": "ชาย",
+        "status": "มีแฟน"
+    },
+    "-LVd1VR1__p_clLub7cM": {
+        "age": "22",
+        "product": "แฟชั่น",
+        "sex": "ชาย",
+        "status": "มีแฟน"
+    },
+    "-LVd1WLY1zwLYEMxiX6I": {
+        "age": "23",
+        "product": "แฟชั่น",
+        "sex": "ชาย",
+        "status": "มีแฟน"
+    },
+    "-LVd1XLIFmdEc8mZyxJ6": {
+        "age": "24",
+        "product": "แฟชั่น",
+        "sex": "ชาย",
+        "status": "มีแฟน"
+    },
+    "-LVd1Y4maHxn8oLYkfY0": {
+        "age": "25",
+        "product": "แฟชั่น",
+        "sex": "ชาย",
+        "status": "มีแฟน"
+    },
+    "-LVd1YpYNuLwpoLAIleX": {
+        "age": "26",
+        "product": "แฟชั่น",
+        "sex": "ชาย",
+        "status": "มีแฟน"
+    },
+    "-LVd1ZkexYfWvRmFRm__": {
+        "age": "27",
+        "product": "แฟชั่น",
+        "sex": "ชาย",
+        "status": "มีแฟน"
+    },
+    "-LVd1_a2-cvXMeE7FgtR": {
+        "age": "28",
+        "product": "แฟชั่น",
+        "sex": "ชาย",
+        "status": "มีแฟน"
+    },
+    "-LVd1a_QWAPW8DLmwPtq": {
+        "age": "29",
+        "product": "แฟชั่น",
+        "sex": "ชาย",
+        "status": "มีแฟน"
+    },
+    "-LVd1cvvT55gX0OmnpBv": {
+        "age": "30",
+        "product": "อุปกรณ์อิเล็กทรอนิกส์",
+        "sex": "ชาย",
+        "status": "มีแฟน"
+    },
+    "-LVd1dqH04K5n9qdXUH5": {
+        "age": "31",
+        "product": "อุปกรณ์อิเล็กทรอนิกส์",
+        "sex": "ชาย",
+        "status": "มีแฟน"
+    },
+    "-LVd1eZQNVLlvKjQ84jh": {
+        "age": "32",
+        "product": "อุปกรณ์อิเล็กทรอนิกส์",
+        "sex": "ชาย",
+        "status": "มีแฟน"
+    },
+    "-LVd1frIaoQSDgAd_uPz": {
+        "age": "33",
+        "product": "ของใช้ในบ้านและเครื่องมือต่างๆ",
+        "sex": "ชาย",
+        "status": "มีแฟน"
+    },
+    "-LVd1i-Q-IWFuGx6Qu_L": {
+        "age": "34",
+        "product": "ของใช้ในบ้านและเครื่องมือต่างๆ",
+        "sex": "ชาย",
+        "status": "มีแฟน"
+    },
+    "-LVd1ijQdA3arrVfHrSJ": {
+        "age": "35",
+        "product": "ของใช้ในบ้านและเครื่องมือต่างๆ",
+        "sex": "ชาย",
+        "status": "มีแฟน"
+    },
+    "-LVd1jaYyzsPdRkgmpXO": {
+        "age": "36",
+        "product": "ของใช้ในบ้านและเครื่องมือต่างๆ",
+        "sex": "ชาย",
+        "status": "มีแฟน"
+    },
+    "-LVd1prm_5IV0uk-cJEb": {
+        "age": "37",
+        "product": "อุปกรณ์อิเล็กทรอนิกส์",
+        "sex": "ชาย",
+        "status": "มีแฟน"
+    },
+    "-LVd1qqAUw1aSjkMDicT": {
+        "age": "38",
+        "product": "อุปกรณ์อิเล็กทรอนิกส์",
+        "sex": "ชาย",
+        "status": "มีแฟน"
+    },
+    "-LVd1s4I6E0dxt_AJDf4": {
+        "age": "39",
+        "product": "ของใช้ในบ้านและเครื่องมือต่างๆ",
+        "sex": "ชาย",
+        "status": "มีแฟน"
+    },
+    "-LVd1t5mGi3IvL_Ht7LF": {
+        "age": "40",
+        "product": "สุขภาพและความงาม",
+        "sex": "ชาย",
+        "status": "มีแฟน"
+    },
+    "-LVd1tnQEbk4F39liQ7r": {
+        "age": "41",
+        "product": "สุขภาพและความงาม",
+        "sex": "ชาย",
+        "status": "มีแฟน"
+    },
+    "-LVd1ur9LPTyu4PPhEoT": {
+        "age": "42",
+        "product": "ของใช้ในบ้านและเครื่องมือต่างๆ",
+        "sex": "ชาย",
+        "status": "มีแฟน"
+    },
+    "-LVd1vkYFtreCPhOL7Az": {
+        "age": "43",
+        "product": "ของใช้ในบ้านและเครื่องมือต่างๆ",
+        "sex": "ชาย",
+        "status": "มีแฟน"
+    },
+    "-LVd1wa216egKeNEhnX5": {
+        "age": "44",
+        "product": "ของใช้ในบ้านและเครื่องมือต่างๆ",
+        "sex": "ชาย",
+        "status": "มีแฟน"
+    },
+    "-LVd2-SfQnVo5P9pgneu": {
+        "age": "12",
+        "product": "เกมและซอฟแวร์",
+        "sex": "หญิง",
+        "status": "โสด"
+    },
+    "-LVd20_1-KBuKjeCaMxw": {
+        "age": "13",
+        "product": "แฟชั่น",
+        "sex": "หญิง",
+        "status": "โสด"
+    },
+    "-LVd21rnzu_L5worwjgg": {
+        "age": "14",
+        "product": "เกมและซอฟแวร์",
+        "sex": "หญิง",
+        "status": "โสด"
+    },
+    "-LVd22bABPW7v2_5fJpB": {
+        "age": "15",
+        "product": "แฟชั่น",
+        "sex": "หญิง",
+        "status": "โสด"
+    },
+    "-LVd23LYhyL4jbRtVvhQ": {
+        "age": "16",
+        "product": "แฟชั่น",
+        "sex": "หญิง",
+        "status": "โสด"
+    },
+    "-LVd23uvh4lIGlju0jKw": {
+        "age": "17",
+        "product": "แฟชั่น",
+        "sex": "หญิง",
+        "status": "โสด"
+    },
+    "-LVd24lf7Hz4PVxMCtCh": {
+        "age": "18",
+        "product": "แฟชั่น",
+        "sex": "หญิง",
+        "status": "โสด"
+    },
+    "-LVd25hQDWC1Lw36y771": {
+        "age": "19",
+        "product": "แฟชั่น",
+        "sex": "หญิง",
+        "status": "โสด"
+    },
+    "-LVd26QPdr4i2S12fFMN": {
+        "age": "20",
+        "product": "แฟชั่น",
+        "sex": "หญิง",
+        "status": "โสด"
+    },
+    "-LVd27fQwwmG7ysd7eLF": {
+        "age": "21",
+        "product": "สุขภาพและความงาม",
+        "sex": "หญิง",
+        "status": "โสด"
+    },
+    "-LVd28MewdW_neg6n7DG": {
+        "age": "22",
+        "product": "สุขภาพและความงาม",
+        "sex": "หญิง",
+        "status": "โสด"
+    },
+    "-LVd29DQyZuwQkOoVZE6": {
+        "age": "23",
+        "product": "สุขภาพและความงาม",
+        "sex": "หญิง",
+        "status": "โสด"
+    },
+    "-LVd2ABn872vreiF9RGD": {
+        "age": "24",
+        "product": "แฟชั่น",
+        "sex": "หญิง",
+        "status": "โสด"
+    },
+    "-LVd2AxAwnqG-TE8K4TD": {
+        "age": "25",
+        "product": "แฟชั่น",
+        "sex": "หญิง",
+        "status": "โสด"
+    },
+    "-LVd2BnAGInZtD7CjqSQ": {
+        "age": "26",
+        "product": "สุขภาพและความงาม",
+        "sex": "หญิง",
+        "status": "โสด"
+    },
+    "-LVd2CufhnO3ZtT0BBdY": {
+        "age": "27",
+        "product": "แฟชั่น",
+        "sex": "หญิง",
+        "status": "โสด"
+    },
+    "-LVd2LdfY5CkCmqsOlAG": {
+        "age": "28",
+        "product": "แฟชั่น",
+        "sex": "หญิง",
+        "status": "โสด"
+    },
+    "-LVd2NFvVFO9sQIpV19B": {
+        "age": "29",
+        "product": "แฟชั่น",
+        "sex": "หญิง",
+        "status": "โสด"
+    },
+    "-LVd2O2IsfMZSU46ppVr": {
+        "age": "30",
+        "product": "แฟชั่น",
+        "sex": "หญิง",
+        "status": "โสด"
+    },
+    "-LVd2VQv0fRWA_EmHhN9": {
+        "age": "50",
+        "product": "ของใช้ในบ้านและเครื่องมือต่างๆ",
+        "sex": "หญิง",
+        "status": "โสด"
+    },
+    "-LVd2WjnbTQVpdyxrXtn": {
+        "age": "51",
+        "product": "ของใช้ในบ้านและเครื่องมือต่างๆ",
+        "sex": "หญิง",
+        "status": "โสด"
+    },
+    "-LVd2XWJpLSRBCg2F00j": {
+        "age": "52",
+        "product": "ของใช้ในบ้านและเครื่องมือต่างๆ",
+        "sex": "หญิง",
+        "status": "โสด"
+    },
+    "-LVd2Znfu16_ZXx689rC": {
+        "age": "12",
+        "product": "เกมและซอฟแวร์",
+        "sex": "หญิง",
+        "status": "มีแฟน"
+    },
+    "-LVd2a6I4HFFC6ugcuGf": {
+        "age": "13",
+        "product": "แฟชั่น",
+        "sex": "หญิง",
+        "status": "มีแฟน"
+    },
+    "-LVd2b3fzm_cMl5M23Z1": {
+        "age": "14",
+        "product": "เกมและซอฟแวร์",
+        "sex": "หญิง",
+        "status": "มีแฟน"
+    },
+    "-LVd2bzfqbuBMIfgmIrE": {
+        "age": "15",
+        "product": "แฟชั่น",
+        "sex": "หญิง",
+        "status": "มีแฟน"
+    },
+    "-LVd2coQjxbevj6j41Mq": {
+        "age": "16",
+        "product": "แฟชั่น",
+        "sex": "หญิง",
+        "status": "มีแฟน"
+    },
+    "-LVd2dU2S44Zk2quyF9u": {
+        "age": "17",
+        "product": "แฟชั่น",
+        "sex": "หญิง",
+        "status": "มีแฟน"
+    },
+    "-LVd2e4IqiPLTbB4sjrT": {
+        "age": "18",
+        "product": "แฟชั่น",
+        "sex": "หญิง",
+        "status": "มีแฟน"
+    },
+    "-LVd2fHQYhmb2bVfBXt4": {
+        "age": "19",
+        "product": "สุขภาพและความงาม",
+        "sex": "หญิง",
+        "status": "มีแฟน"
+    },
+    "-LVd2fuuiNccVNmAAhF9": {
+        "age": "20",
+        "product": "สุขภาพและความงาม",
+        "sex": "หญิง",
+        "status": "มีแฟน"
+    },
+    "-LVd2gVYurskuydgXW7w": {
+        "age": "21",
+        "product": "สุขภาพและความงาม",
+        "sex": "หญิง",
+        "status": "มีแฟน"
+    },
+    "-LVd2oOQP0oU8xX7QPjz": {
+        "age": "22",
+        "product": "แฟชั่น",
+        "sex": "หญิง",
+        "status": "มีแฟน"
+    },
+    "-LVd2p6IceTyLo3BYApp": {
+        "age": "23",
+        "product": "แฟชั่น",
+        "sex": "หญิง",
+        "status": "มีแฟน"
+    },
+    "-LVd2q7XJ-SwzPneOY9i": {
+        "age": "24",
+        "product": "สุขภาพและความงาม",
+        "sex": "หญิง",
+        "status": "มีแฟน"
+    },
+    "-LVd2rXvA9F4yzDicZvw": {
+        "age": "25",
+        "product": "แฟชั่น",
+        "sex": "หญิง",
+        "status": "มีแฟน"
+    },
+    "-LVd2sEYuecWfNmnHXJ2": {
+        "age": "26",
+        "product": "แฟชั่น",
+        "sex": "หญิง",
+        "status": "มีแฟน"
+    },
+    "-LVd2tEXY4n-CewKwlqa": {
+        "age": "27",
+        "product": "สุขภาพและความงาม",
+        "sex": "หญิง",
+        "status": "มีแฟน"
+    },
+    "-LVd2uOIhJ_EMdWNAhvt": {
+        "age": "28",
+        "product": "แฟชั่น",
+        "sex": "หญิง",
+        "status": "มีแฟน"
+    },
+    "-LVd2vAfSRWFAJMHaG7T": {
+        "age": "29",
+        "product": "แฟชั่น",
+        "sex": "หญิง",
+        "status": "มีแฟน"
+    },
+    "-LVd2vvQnCw3mxJKME8f": {
+        "age": "30",
+        "product": "แฟชั่น",
+        "sex": "หญิง",
+        "status": "มีแฟน"
+    },
+    "-LVd2x6vukqoBuG3CmQx": {
+        "age": "31",
+        "product": "ของใช้ในบ้านและเครื่องมือต่างๆ",
+        "sex": "หญิง",
+        "status": "มีแฟน"
+    },
+    "-LVd2yEItf1oMOUkW4us": {
+        "age": "32",
+        "product": "สุขภาพและความงาม",
+        "sex": "หญิง",
+        "status": "มีแฟน"
+    },
+    "-LVd2z2QT0yjsi26Ef7b": {
+        "age": "33",
+        "product": "ของใช้ในบ้านและเครื่องมือต่างๆ",
+        "sex": "หญิง",
+        "status": "มีแฟน"
+    },
+    "-LVd3-1fgVtqQR6a_sAi": {
+        "age": "34",
+        "product": "แฟชั่น",
+        "sex": "หญิง",
+        "status": "มีแฟน"
+    },
+    "-LVd3-vm-mUC780PhV9F": {
+        "age": "35",
+        "product": "ของใช้ในบ้านและเครื่องมือต่างๆ",
+        "sex": "หญิง",
+        "status": "มีแฟน"
+    },
+    "-LVd31Ev5VSr-kqqyBJv": {
+        "age": "36",
+        "product": "ของใช้ในบ้านและเครื่องมือต่างๆ",
+        "sex": "หญิง",
+        "status": "มีแฟน"
+    },
+    "-LVd33R2lptsk-7KMigG": {
+        "age": "37",
+        "product": "สุขภาพและความงาม",
+        "sex": "หญิง",
+        "status": "มีแฟน"
+    },
+    "-LVd34tYXvbnYKpuL-84": {
+        "age": "38",
+        "product": "แฟชั่น",
+        "sex": "หญิง",
+        "status": "มีแฟน"
+    },
+    "-LVd3622VRR4qe5tzAJc": {
+        "age": "39",
+        "product": "ของใช้ในบ้านและเครื่องมือต่างๆ",
+        "sex": "หญิง",
+        "status": "มีแฟน"
+    },
+    "-LVd376IGXnZkf4zR4xo": {
+        "age": "40",
+        "product": "ของใช้ในบ้านและเครื่องมือต่างๆ",
+        "sex": "หญิง",
+        "status": "มีแฟน"
+    },
+    "-LVd38sQ3dv7W1Oz9VDk": {
+        "age": "41",
+        "product": "แฟชั่น",
+        "sex": "หญิง",
+        "status": "มีแฟน"
+    },
+    "-LVd39xAocAoGVct2jWl": {
+        "age": "42",
+        "product": "ของใช้ในบ้านและเครื่องมือต่างๆ",
+        "sex": "หญิง",
+        "status": "มีแฟน"
+    },
+    "-LVd3BYfJzLnmxNP7oVT": {
+        "age": "43",
+        "product": "ของใช้ในบ้านและเครื่องมือต่างๆ",
+        "sex": "หญิง",
+        "status": "มีแฟน"
+    },
+    "-LVd3C0e6n51mGvqOQ3Y": {
+        "age": "44",
+        "product": "ของใช้ในบ้านและเครื่องมือต่างๆ",
+        "sex": "หญิง",
+        "status": "มีแฟน"
+    }
+}
--- scr/new_interest_product.js
+++ scr/new_interest_product.js
@@ -1,24 +1,34 @@
-let dataJ = d; //from form-interest-product-export.js
+let dataJ = DATA; //from form-interest-product-export.js
 let dataAll = [];
 let interest = [];  
 let keys = Object.keys(dataJ);
-let labalProduct = ['อุปกรณ์คอม', 'อาหารเสริม', 'แฟชั่น', 'ของใช้ในบ้าน'];
+const numExamples = keys.length;
+tf.util.shuffle(keys);
+let labalProduct = ['อุปกรณ์อิเล็กทรอนิกส์', 'สุขภาพและความงาม', 'แฟชั่น', 'ของใช้ในบ้านและเครื่องมือต่างๆ', 'เกมและซอฟแวร์'];
+let numClass = labalProduct.length;
 for (let key of keys) {
     let val = dataJ[key];
     let ageInt = parseInt(val.age)
-    let ageFlost = ageInt / 60;
-    let keySex = val.sex === 'ชาย' ? 0 : 1 
-    let keyLW = val.l_or_w === 'เรียน' ? 0 : 1
-    let keyStatus = val.status === 'โสด' ? 0 : 1
-    let keyProduct = val.product === 'อุปกรณ์คอม' ? 0 : 
-    val.product === 'อาหารเสริม' ? 1 : 
-    val.product === 'แฟชั่น' ? 2 : 3 // 3 = ของใช้ในบ้าน
-    let d = [ageFlost, keySex, keyLW, keyStatus]
+    let ageFlost = ageInt / 10;
+    let agetoFixed = ageFlost.toFixed(1);
+    let age = parseFloat(agetoFixed);
+    let keySex = val.sex === 'ชาย' ? 0 : 0.1 
+    let keyStatus = val.status === 'โสด' ? 0 : 0.2
+    let keyProduct = val.product === 'อุปกรณ์อิเล็กทรอนิกส์' ? 0 
+        : val.product === 'สุขภาพและความงาม' ? 1 
+        : val.product === 'แฟชั่น' ? 2 
+        : val.product === 'ของใช้ในบ้านและเครื่องมือต่างๆ' ? 3 
+        : 4 // 4 = เกมและซอฟแวร์
+    let sex = age * keySex;
+    let status = age * keyStatus;
+    let d = [age, sex, status]
     dataAll.push(d)
     interest.push(keyProduct);
 }
 
-// console.log(dataAll);
+const numTestExamples = Math.round(numExamples * 0.8);
+const numTrainExamples = numExamples - numTestExamples;
+const xDims = dataAll[0].length;
 // console.log(interest);
 
 //ออกแบบ tensor xs, ys
@@ -28,27 +38,44 @@ let xs = tf.tensor2d(dataAll);
 let interest1D = tf.tensor1d(interest, 'int32');
 // interest1D.print();
 
-let ys = tf.oneHot(interest1D, 4).cast('float32');
+let ys = tf.oneHot(interest1D, numClass).cast('float32');
 // ys.print();
 interest1D.dispose();
+
+const xTrain = xs.slice([0, 0], [numTrainExamples, xDims]);
+const xTest = xs.slice([numTrainExamples, 0], [numTestExamples, xDims]);
+const yTrain = ys.slice([0, 0], [numTrainExamples, numClass]);
+const yTest = ys.slice([0, 0], [numTestExamples, numClass]);
+
+const xTrains = [];
+const yTrains = [];
+const xTests = [];
+const yTests = [];
+for (let i = 0; i < numClass.length; ++i) {
+    xTrains.push(xTrain);
+    yTrains.push(yTrain);
+    xTests.push(xTest);
+    yTests.push(yTest);
+}
 
 let model = tf.sequential();
 
 const hidden = tf.layers.dense({
-    units: 4,
-    inputShape: [4],
+    units: 50,
+    inputShape: [xDims],
     activation: 'sigmoid'
 });
 
 const output = tf.layers.dense({
-    units: 4,
+    units: numClass,
     activation: 'softmax'
 });
 
 model.add(hidden);
 model.add(output);
+model.summary();
 
-let optimizer = tf.train.sgd(0.7);
+let optimizer = tf.train.adam(0.01);
 model.compile({
     optimizer: optimizer,
     loss: 'categoricalCrossentropy',
@@ -56,15 +83,18 @@ model.compile({
 });
 
 async function train() {
-    await model.fit(xs, ys, {
-        validationSplit: 0.06,
-        epochs: 1000, 
+    let acc;
+    await model.fit(xTrain, yTrain, {
+        epochs: 1000,
+        validationData: [xTest, yTest], 
         callbacks: {
             onEpochEnd: (e, l) => {
-                console.log(e, "|", l.loss);
+                console.log(e, '|', l.acc);
+                // acc = l.acc;
             },
             onTrainEnd: () => {
                 console.log("เทรดเสร็จแล้ว");
+                // console.log(acc);
             }
         }
     });
@@ -76,15 +106,17 @@ function getQQ() {
     let getAge = document.getElementById('age').value;
     let getSex = document.getElementById('sex').value;
     let getStatus = document.getElementById('status').value;
-    let getLorW =  document.getElementById('l_or_w').value;
 
     let getAgeInt = parseInt(getAge);
-    let getKeySex = getSex === 'ชาย' ? 0 : 1 ;
-    let getKeyLW = getLorW === 'เรียน' ? 0 : 1;
-    let getKeyStatus = getStatus === 'โสด' ? 0 : 1;
-
+    let getKeySex = getSex === 'ชาย' ? 0 : 0.1 ;
+    let getKeyStatus = getStatus === 'โสด' ? 0 : 0.2;
+    let getAgeFloat = getAgeInt / 10;
+    let agetoFixed = getAgeFloat.toFixed(1);
+    let getage = parseFloat(agetoFixed);
+    let sex = getage * getKeySex;
+    let status = getage * getKeyStatus;
     let test2D = [
-        [getAgeInt / 60, getKeySex, getKeyLW, getKeyStatus]
+        [getage, sex, status]
     ]
 
     let totensor2D = tf.tensor2d(test2D);
